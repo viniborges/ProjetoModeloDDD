56d04e92c7237ce84c796e60aa280279fcb4bb6c
viniborges <viniborges@yahoo.com.br>
2018-06-26 22:20:18 -0300

    Ajuste na arquitetura DDD. Adicionado EF.

---
+++ ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+
+        public  string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public  string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            //Retorna true quando o cliente for especial
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -0,0 +1,15 @@
+﻿
+using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    //Contexto do banco de dados do Entity
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext() : base("ProjetoModeloDDD") { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
