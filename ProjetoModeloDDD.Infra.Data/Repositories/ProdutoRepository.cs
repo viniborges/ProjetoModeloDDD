97a00bffcbdc7bccdecfbf3c9abc75d444044f57
viniborges <viniborges@yahoo.com.br>
2018-07-18 19:20:22 -0300

    Adicionado ViewModels de Produto e Cliente. Adicionado referência AutoMapper.

--- ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
+++ ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System.Collections.Generic;
+using System.Linq;
 using ProjetoModeloDDD.Domain.Entities;
 using ProjetoModeloDDD.Domain.Interfaces;
 
@@ -10,7 +11,7 @@ namespace ProjetoModeloDDD.Infra.Data.Repositories
     {
         public IEnumerable<Produto> BuscarPorNome(string nome)
         {
-            throw new System.NotImplementedException();
+            return Db.Produtos.Where(p => p.Nome == nome);
         }
     }
 }
---
+++ ProjetoModeloDDD.MVC/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,30 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoModeloDDD.MVC.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo nome")]
+        [MaxLength(150, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha um valor")]
+        [DataType(DataType.Currency)]
+        [Range(typeof(decimal), "0", "99999999999")]
+        public decimal Valor { get; set; }
+
+        [DisplayName("Disponível?")]
+        public bool Disponivel { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public virtual ClienteViewModel Cliente { get; set; }
+
+    }
+}
