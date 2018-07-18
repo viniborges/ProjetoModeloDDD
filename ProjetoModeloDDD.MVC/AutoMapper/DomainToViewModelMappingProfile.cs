0c4a03d2e14a626790b45a7b3cc5982b21c4cb8b
viniborges <viniborges@yahoo.com.br>
2018-07-18 20:19:55 -0300

    Configurado AutoMapper e feeito contgroller/view de criação de clientes para teste.

--- ProjetoModeloDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
+++ ProjetoModeloDDD.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using AutoMapper;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
 
 namespace ProjetoModeloDDD.MVC.AutoMapper
 {
-    public class DomainToViewModelMappingProfile
+    public class DomainToViewModelMappingProfile : Profile
     {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel>();
+            Mapper.CreateMap<Produto, ProdutoViewModel>();
+        }
     }
 }
