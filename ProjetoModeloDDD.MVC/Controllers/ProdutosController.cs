72481ff676caee1028ddd2c1b520aa436beec73b
viniborges <viniborges@yahoo.com.br>
2018-07-25 02:11:03 -0300

    Configurado ninject e criado controllers e views de Clientes.

---
+++ ProjetoModeloDDD.MVC/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoModeloDDD.MVC.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
